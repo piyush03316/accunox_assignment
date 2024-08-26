b237c6055db413d3dcb32f624878ae2c92a46d70
piyush03316 <piyush1211003@gmail.com>
2024-08-26 15:59:05 +0000

    Update App.jsx

--- `Src/App.jsx
+++ `Src/App.jsx
@@ -1,1 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './screens/home';
+
+// store widget in json and then render the card 
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
 
